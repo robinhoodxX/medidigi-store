753ab6bc059919b95cbeccba7365f27f044e2255
Haider Zaman Khan <59317216+robinhoodxX@users.noreply.github.com>
2026-02-09 21:16:58 +0500

    updated panel

--- src/app/homepage/dashboard/components/leftmenudash.jsx
+++ src/app/homepage/dashboard/components/leftmenudash.jsx
@@ -61,7 +61,7 @@ export default function leftmenudash() {
         >
           <Tab label="Search" {...a11yProps(0)} />
           <Tab label="Whistlist" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
+          <Tab label="Profile" {...a11yProps(2)} />
           <Tab label="Item Four" {...a11yProps(3)} />
           <Tab label="Item Five" {...a11yProps(4)} />
           <Tab label="Item Six" {...a11yProps(5)} />
