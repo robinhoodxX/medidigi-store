ec391cb8932005b84db5220a253ae9e86525cb19
Haider Zaman Khan <59317216+robinhoodxX@users.noreply.github.com>
2026-02-17 15:07:50 +0500

    Updated the customs page with new input fields and a search button.

--- src/app/homepage/dashboard/components/customs/page.jsx
+++ src/app/homepage/dashboard/components/customs/page.jsx
@@ -1,17 +1,47 @@
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import { Box, Typography } from '@mui/material';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { Box, Typography } from "@mui/material";
 
 export default function customs() {
   return (
-    <Box sx={{ p: 2, bgcolor: '#f5f5f5', borderRadius: 1, mt: 2, mb: 2 }} >
+    <Box sx={{ p: 2, bgcolor: "#f5f5f5", borderRadius: 1, m: 20 }}>
       <Box>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            mb: 2,
+            p: 2,
+            borderRadius: 1,
+          }}
+        >
+          <TextField id="outlined-basic" label="Drug name" variant="outlined" />
+          <TextField id="outlined-basic" label="Alias" variant="outlined" />
+          <TextField id="outlined-basic" label="Category" variant="outlined" />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            mb: 2,
+            p: 2,
+            borderRadius: 1,
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Dosage form"
+            variant="outlined"
+          />
+          <TextField id="outlined-basic" label="Effect" variant="outlined" />
+          <TextField id="outlined-basic" label="Keywords" variant="outlined" />
+        </Box>
       </Box>
-      <Button>Secondary Button</Button>
+      <Box>
+        <Button variant="outlined">Search</Button>
+      </Box>
     </Box>
   );
 }
-
