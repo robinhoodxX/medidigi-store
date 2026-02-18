d38c9b433d4f0daa5dd084b16e8981563db3f28d
Haider Zaman Khan <59317216+robinhoodxX@users.noreply.github.com>
2026-02-18 12:54:44 +0500

    updated

--- src/app/homepage/dashboard/components/customs/page.jsx
+++ src/app/homepage/dashboard/components/customs/page.jsx
@@ -5,6 +5,12 @@ import { Box, Typography } from "@mui/material";
 export default function customs() {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", p: 2, bgcolor: "#f5f5f5", borderRadius: 1, m: 20 }}>
+      <Typography variant="h4" gutterBottom>
+        Custom Drug Search
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Enter specific drug attributes to find the perfect match for your needs.
+      </Typography>
       <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", mb: 4 }}>
         <Box sx={{ display: "flex", gap: 2, mb: 2 }}>
           <TextField id="outlined-basic" label="Drug name" variant="outlined" />
